51d4ca5c1b4b5bc88c7e80fc9749d0d5908704d6
PhonsterMan <jakekkeating@gmail.com>
2025-04-19 17:40:53 +1000

    forgot to import cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 // allows requests from any url
 app.use(cors()); 
