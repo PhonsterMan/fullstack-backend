3dcea87aad4f909c3602f391a3e528414e526916
PhonsterMan <jakekkeating@gmail.com>
2025-04-19 17:50:05 +1000

    added increment option

--- src/index.js
+++ src/index.js
@@ -9,6 +9,13 @@ app.get('/data', (req, res) => {
   res.sendFile(path.join(__dirname, 'data.json'));
 });
 
+app.post('/increment', (req, res) => {
+  let data = JSON.parse(fs.readFileSync('data.json'));
+  data.counter++;
+  fs.writeFileSync('data.json', JSON.stringify(data));
+  res.json(data);
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
