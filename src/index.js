96f16f18b6c1c9ce50ef8b7dd3fef14104e10718
PhonsterMan <jakekkeating@gmail.com>
2025-04-19 18:01:23 +1000

    added fs to change varible when incremented

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const fs = require('fs');
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 // allows requests from any url
